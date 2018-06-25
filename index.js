0d329ca66c3fe1380a6172560657da409db3eab9
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-25 11:43:24 -0400

    Fix server serving issue

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/client/build/static`));
+app.use(express.static(`${__dirname}/client/build`));
 
 app.get('/', (req, res) => {
 	res.end();
