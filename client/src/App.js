692566eaf0007f06478e8f498866f76ecd0c8ba8
Patricio de la Guardia <padlgt01@gmail.com>
2018-06-25 15:01:21 -0400

    Add extra code to server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+	async componentDidMount() {
+		let {data} = await axios.get('/api/questions').catch(e => console.log(e));
+		data.results.forEach((question) => {
+			question.correctAnswer = question.correct_answer;
+			question.incorrectAnswers = question.incorrect_answers;
+			delete question.correct_answer;
+			delete question.incorrect_answers;
+		})
+	
+		console.log(data.results);
+	}
+
   render() {
     return (
       <div className="App">
